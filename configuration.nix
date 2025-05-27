913f266bcf86844a7900ff014248e226af42a792
juan_ma7u7 <jmlafuente97@gmail.com>
2025-05-27 13:02:28 -0600

    samba

--- configuration.nix
+++ configuration.nix
@@ -38,9 +38,6 @@ in
         "server string" = "smbnix";
         "netbios name" = "smbnix";
         "security" = "user";
-        "path" = "/home/juan_ma7u7";
-        "browseable" = "yes";
-        "read only" = "no";
         #"use sendfile" = "yes";
         #"max protocol" = "smb2";
         # note: localhost is the ipv6 localhost ::1
@@ -49,26 +46,16 @@ in
         "guest account" = "nobody";
         "map to guest" = "bad user";
       };
-      # "public" = {
-      #   "path" = "/mnt/Shares/Public";
-      #   "browseable" = "yes";
-      #   "read only" = "no";
-      #   "guest ok" = "yes";
-      #   "create mask" = "0644";
-      #   "directory mask" = "0755";
-      #   "force user" = "username";
-      #   "force group" = "groupname";
-      # };
-      # "private" = {
-      #   "path" = "/home/juan_ma7u7";
-      #   "browseable" = "yes";
-      #   "read only" = "no";
-      #   "guest ok" = "no";
-      #   "create mask" = "0644";
-      #   "directory mask" = "0755";
-      #   "force user" = "username";
-      #   "force group" = "groupname";
-      # };
+      "NixOS-Share" = {
+        "path" = "/home/juan_ma7u7";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "username";
+        "force group" = "groupname";
+      };
     };
   };
 
