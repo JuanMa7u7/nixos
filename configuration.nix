d2aeccc61c41ec9cced22d0bc4c781badf1c4524
juan_ma7u7 <jmlafuente97@gmail.com>
2025-05-27 12:33:42 -0600

    samba

--- configuration.nix
+++ configuration.nix
@@ -23,7 +23,55 @@ in
   # Firewall
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [80 443 3000];
+    allowedTCPPorts = [80 443 445 3000];
+    allowPing = true;
+  };
+
+  # Samba
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    openFirewall = true;
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "smbnix";
+        "netbios name" = "smbnix";
+        "security" = "user";
+        #"use sendfile" = "yes";
+        #"max protocol" = "smb2";
+        # note: localhost is the ipv6 localhost ::1
+        "hosts allow" = "192.168.117. 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+      };
+      # "public" = {
+      #   "path" = "/mnt/Shares/Public";
+      #   "browseable" = "yes";
+      #   "read only" = "no";
+      #   "guest ok" = "yes";
+      #   "create mask" = "0644";
+      #   "directory mask" = "0755";
+      #   "force user" = "username";
+      #   "force group" = "groupname";
+      # };
+      "private" = {
+        "path" = "/home/juan_ma7u7";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "username";
+        "force group" = "groupname";
+      };
+    };
+  };
+
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
   };
 
   # Set pkgs for hydenix globally, any file that imports pkgs will use this
