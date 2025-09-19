c4d7c1761eaab64dbfc743ba299fa32e14ea8fe0
juan_ma7u7 <jmlafuente97@gmail.com>
2025-09-19 13:24:56 -0600

    fix

--- modules/hm/default.nix
+++ modules/hm/default.nix
@@ -33,7 +33,7 @@
     fastfetch.enable = true; # fastfetch configuration
     firefox = {
       enable = true; # enable firefox module
-      useHydeConfig = false; # use hyde firefox configuration and extensions
+      # useHydeConfig = false; # use hyde firefox configuration and extensions
       useUserChrome = true; # if useHydeConfig is true, apply hyde userChrome CSS customizations
       useUserJs = true; # if useHydeConfig is true, apply hyde user.js preferences
       # useExtensions = true; # if useHydeConfig is true, install hyde firefox extensions
