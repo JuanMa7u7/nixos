d6310136eeba62372c4f886004fee2b9790904ba
juan_ma7u7 <jmlafuente97@gmail.com>
2026-03-03 17:22:03 -0600

    fix

--- hosts/thinkpad-l15/home.nix
+++ hosts/thinkpad-l15/home.nix
@@ -1,26 +1,12 @@
-{ pkgs, ... }:
-let
-  pyamdgpuinfo = pkgs.python3Packages.pyamdgpuinfo or null;
-  amdGpuPython =
-    pkgs.lib.optional (pyamdgpuinfo != null)
-      (pkgs.python3.withPackages (ps: [ ps.pyamdgpuinfo ]));
-in
+{ pkgs, ... }: 
 {
   hydenix.hm.theme.active = "Gruvbox Retro";
-
-  home.packages = amdGpuPython;
 
   home.file = {
     ".config/hypr/monitors.conf" = pkgs.lib.mkForce {
       source = ../../modules/hm/confs/hosts/thinkpad-l15/monitors.conf;
       force = true;
       mutable = false;
-    };
-
-    ".local/lib/hyde/amdgpu.py" = pkgs.lib.mkForce {
-      source = ../../modules/hm/confs/amdgpu.py;
-      force = true;
-      mutable = true;
     };
   };
 }
