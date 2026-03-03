e8d2a3750a134d4c3f501feb1abf2aeabe5adc84
Juan Manuel Lafuente Araiza <jmlafuente97@gmail.com>
2026-03-02 21:20:54 -0600

    enable desktop mounting support for ntfs and exfat drives

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -5,6 +5,15 @@
     # ./example.nix - add your modules here
   ];
 
+  boot.supportedFilesystems = [ "ntfs" "exfat" ];
+
+  security.polkit.enable = true;
+
+  services = {
+    gvfs.enable = true;
+    udisks2.enable = true;
+  };
+
   environment.systemPackages = [
     # pkgs.vscode - hydenix's vscode version
     # pkgs.userPkgs.vscode - your personal nixpkgs version
