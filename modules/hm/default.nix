644e078e36bf03dd5f9c3740abd8267d068bc997
juan_ma7u7 <jmlafuente97@gmail.com>
2026-03-05 11:39:24 -0600

    zen browser as default web browser

--- modules/hm/default.nix
+++ modules/hm/default.nix
@@ -8,6 +8,17 @@
 
   home.packages = [ ];
 
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = [ "zen-beta.desktop" ];
+      "x-scheme-handler/http" = [ "zen-beta.desktop" ];
+      "x-scheme-handler/https" = [ "zen-beta.desktop" ];
+      "x-scheme-handler/about" = [ "zen-beta.desktop" ];
+      "x-scheme-handler/unknown" = [ "zen-beta.desktop" ];
+    };
+  };
+
   hydenix.hm = {
     enable = true;
     comma.enable = true;
