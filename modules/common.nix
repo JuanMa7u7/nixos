3187290ef987452992987b08ac57458cba2380bd
Juan Manuel Lafuente Araiza <jmlafuente97@gmail.com>
2026-03-05 20:37:40 -0600

    fix xbox controller bluetooth

--- modules/common.nix
+++ modules/common.nix
@@ -17,6 +17,29 @@
     PRISMA_INTROSPECTION_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/introspection-engine";
   };
 
+  boot.extraModprobeConfig = ''
+    options bluetooth disable_ertm=Y
+    options btusb enable_autosuspend=n
+  '';
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = true;
+        JustWorksRepairing = "always";
+      };
+      Policy = {
+        AutoEnable = true;
+      };
+    };
+  };
+
+  hardware.xpadneo.enable = true;
+  services.blueman.enable = true;
+
   users.users."juan_ma7u7" = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "video" "docker" ];
